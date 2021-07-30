55850681af9a70cecf739128d92ee0197e97b2af
GuangzheWen <jt20452@bristol.ac.uk>
2021-07-30 23:37:00 +0100

    set local static mock data first

---
+++ PetStore/src/app/mockData/pets.ts
@@ -0,0 +1,41 @@
+import { Pet } from '../models/pet'
+
+export const pets: Pet[] = [
+    {
+        id: 10000001,
+        category: {
+            id: 0,
+            name: 'cat'
+            },
+        name: 'test01',
+        photoUrls: [
+                'string'
+            ],
+        tags: [
+                {
+                    id: 0,
+                    name: 'string'
+                }
+            ],
+        status: 'team2'
+    },
+    {
+        id: 10000002,
+        category: {
+            id: 0,
+            name: 'cat'
+            },
+        name: 'test02',
+        photoUrls: [
+                'string'
+            ],
+        tags: [
+                {
+                    id: 0,
+                    name: 'string'
+                }
+            ],
+        status: 'team2'
+    }
+
+]
