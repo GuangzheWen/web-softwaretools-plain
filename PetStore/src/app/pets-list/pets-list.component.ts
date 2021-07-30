b6fcf04bd564612e37b403357b2257332f018829
GuangzheWen <jt20452@bristol.ac.uk>
2021-07-31 00:14:15 +0100

    some code about pet details in list component temporarily

--- PetStore/src/app/pets-list/pets-list.component.ts
+++ PetStore/src/app/pets-list/pets-list.component.ts
@@ -8,9 +8,20 @@ import { pets } from '../mockData/pets';
   templateUrl: './pets-list.component.html',
   styleUrls: ['./pets-list.component.css']
 })
+
+// This is a List component for displaying pets.
+// But not store data, only for View in MVC.
+
 export class PetsListComponent implements OnInit {
 
+  // mock static data 
   pets: Pet[] = pets
+  selectedPet?: Pet
+
+  onSelect(pet: Pet){
+    this.selectedPet = pet
+    console.log(1)
+  }
 
   constructor() { }
 
