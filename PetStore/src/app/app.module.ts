adde87bdbd6d2e5eeb82511b655d3006876d585c
GuangzheWen <jt20452@bristol.ac.uk>
2021-07-30 23:50:17 +0100

    import FormsModule

--- PetStore/src/app/app.module.ts
+++ PetStore/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetsListComponent } from './pets-list/pets-list.component';
 
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { PetsListComponent } from './pets-list/pets-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
